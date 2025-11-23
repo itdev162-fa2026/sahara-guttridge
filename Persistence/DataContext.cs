7626448451b14f79b2a394e440da9f46223e14c4
Sahara Guttridge <guttrids@gmatc.matc.edu>
2025-11-23 17:01:57 -0600

    Complete Activity 10: Order Checkout Frontend

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -16,6 +16,9 @@ namespace Persistence
       var folder = Environment.SpecialFolder.LocalApplicationData;
       var path = Environment.GetFolderPath(folder);
       DbPath = System.IO.Path.Join(path, "Blogbox.db");
+      
+    
+
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
