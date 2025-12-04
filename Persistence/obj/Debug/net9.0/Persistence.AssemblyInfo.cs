39b4a0e681f47c331eca029ac7309d7192f1ae5a
Sahara Guttridge <guttrids@gmatc.matc.edu>
2025-12-03 23:52:03 -0600

    Activity 11

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ebfe3abcbdade8e91f837ce63123c7f87df0c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf1997422328d19ace6b767c7c5868bdc89b337")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
